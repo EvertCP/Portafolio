37bb2870ee192d236cbb50e7474d7cdb61653bbb
Evert <evert_carpo@hotmail.com>
2026-04-26 10:41:17 -0600

    fix error

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { FaExternalLinkAlt, FaGithub } from 'react-icons/fa';
+import { FaExternalLinkAlt } from 'react-icons/fa';
 
 interface Project {
   id: number;
